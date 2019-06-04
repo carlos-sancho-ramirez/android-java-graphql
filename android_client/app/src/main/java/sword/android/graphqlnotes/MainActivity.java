711c182956611c5eb9cee1170003b85445620f23
Carlos Sancho Ramirez <carlos.sancho.ramirez@gmail.com>
2019-06-04 16:42:54 +0200

    Allow creating new notes

--- android_client/app/src/main/java/sword/android/graphqlnotes/MainActivity.java
+++ android_client/app/src/main/java/sword/android/graphqlnotes/MainActivity.java
@@ -1,10 +1,16 @@
 package sword.android.graphqlnotes;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Toast;
 
@@ -15,12 +21,11 @@ import com.apollographql.apollo.exception.ApolloException;
 
 import org.jetbrains.annotations.NotNull;
 
-import java.net.Proxy;
 import java.util.ArrayList;
 
 import okhttp3.OkHttpClient;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public final class MainActivity extends AppCompatActivity implements View.OnClickListener, DialogInterface.OnClickListener, TextWatcher {
 
     private static ApolloClient mApolloClient;
 
@@ -36,6 +41,19 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         return mApolloClient;
     }
 
+    interface States {
+        int READY = 0;
+        int CREATING_NOTE = 1;
+    }
+
+    interface SavedKeys {
+        String STATE = "st";
+        String PROPOSED_TITLE = "pt";
+    }
+
+    private int mState;
+    private String mProposedTitle;
+
     private ListView mListView;
 
     @Override
@@ -49,6 +67,58 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         fab.setOnClickListener(this);
 
         mListView = findViewById(R.id.listView);
+
+        if (savedInstanceState != null) {
+            mState = savedInstanceState.getInt(SavedKeys.STATE);
+            mProposedTitle = savedInstanceState.getString(SavedKeys.PROPOSED_TITLE);
+        }
+
+        if (mState == States.CREATING_NOTE) {
+            displayCreateNoteDialog();
+        }
+        else {
+            queryAllNotes();
+        }
+    }
+
+    private void displayCreateNoteDialog() {
+        final AlertDialog dialog = new AlertDialog.Builder(this)
+                .setTitle("Type the new note title")
+                .setPositiveButton("Create", this)
+                .setNegativeButton("Cancel", this)
+                .setCancelable(false)
+                .create();
+
+        final View view = LayoutInflater.from(dialog.getContext()).inflate(R.layout.create_note_dialog, null);
+        final EditText editText = view.findViewById(R.id.editText);
+        editText.setText(mProposedTitle);
+        editText.addTextChangedListener(this);
+        dialog.setView(view);
+        dialog.show();
+    }
+
+    @Override
+    public void onClick(DialogInterface dialog, int which) {
+        switch (which) {
+            case DialogInterface.BUTTON_POSITIVE:
+                mState = States.READY;
+                final String title = mProposedTitle;
+                mProposedTitle = null;
+                createNote(title);
+                break;
+
+            case DialogInterface.BUTTON_NEGATIVE:
+                mState = States.READY;
+                mProposedTitle = null;
+
+                if (mListView.getAdapter() == null) {
+                    queryAllNotes();
+                }
+                break;
+        }
+    }
+
+    private void queryAllNotes() {
         getApolloClient().query(AllNotesQuery.builder().build()).enqueue(new ApolloCall.Callback<AllNotesQuery.Data>() {
             @Override
             public void onResponse(@NotNull Response<AllNotesQuery.Data> response) {
@@ -80,8 +150,57 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         });
     }
 
+    private void createNote(String title) {
+        getApolloClient().mutate(CreateNoteMutation.builder().title(title).build()).enqueue(new ApolloCall.Callback<CreateNoteMutation.Data>() {
+            @Override
+            public void onResponse(@NotNull Response<CreateNoteMutation.Data> response) {
+                runOnUiThread(new Runnable() {
+
+                    @Override
+                    public void run() {
+                        queryAllNotes();
+                    }
+                });
+            }
+
+            @Override
+            public void onFailure(@NotNull ApolloException e) {
+                runOnUiThread(new Runnable() {
+
+                    @Override
+                    public void run() {
+                        Toast.makeText(MainActivity.this, "Something went wrong", Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+        });
+    }
+
     @Override
     public void onClick(View v) {
-        Toast.makeText(this, "To be implemented", Toast.LENGTH_SHORT).show();
+        mState = States.CREATING_NOTE;
+        displayCreateNoteDialog();
+    }
+
+    @Override
+    public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+        // Nothing to be done
+    }
+
+    @Override
+    public void onTextChanged(CharSequence s, int start, int before, int count) {
+        // Nothing to be done
+    }
+
+    @Override
+    public void afterTextChanged(Editable s) {
+        mProposedTitle = s.toString();
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt(SavedKeys.STATE, mState);
+        outState.putString(SavedKeys.PROPOSED_TITLE, mProposedTitle);
     }
 }
