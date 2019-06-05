490dc4152c9ead4728f02460b1c53ca7ff25e86e
Carlos Sancho Ramirez <carlos.sancho.ramirez@gmail.com>
2019-06-05 17:30:09 +0200

    Android client can save notes

--- android_client/app/src/main/java/sword/android/graphqlnotes/NoteActivity.java
+++ android_client/app/src/main/java/sword/android/graphqlnotes/NoteActivity.java
@@ -3,7 +3,11 @@ package sword.android.graphqlnotes;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.StringRes;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -32,6 +36,8 @@ public final class NoteActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_note);
 
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
         mEditText = findViewById(R.id.editText);
 
         queryNoteContent();
@@ -56,13 +62,51 @@ public final class NoteActivity extends AppCompatActivity {
 
             @Override
             public void onFailure(@NotNull ApolloException e) {
-                runOnUiThread(new Runnable() {
+                showFeedback(R.string.error);
+            }
+        });
+    }
 
-                    @Override
-                    public void run() {
-                        Toast.makeText(NoteActivity.this, "Something went wrong", Toast.LENGTH_SHORT).show();
-                    }
-                });
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.note, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.saveOption:
+                saveNote();
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void saveNote() {
+        final String noteId = getIntent().getStringExtra(ArgKeys.ID);
+        final String text = mEditText.getText().toString();
+        MainActivity.getApolloClient().mutate(UpdateNoteMutation.builder().noteId(noteId).body(text).build()).enqueue(new ApolloCall.Callback<UpdateNoteMutation.Data>() {
+            @Override
+            public void onResponse(@NotNull Response<UpdateNoteMutation.Data> response) {
+                showFeedback(R.string.noteSavedFeedback);
+            }
+
+            @Override
+            public void onFailure(@NotNull ApolloException e) {
+                showFeedback(R.string.error);
+            }
+        });
+    }
+
+    private void showFeedback(@StringRes int text) {
+        final int toastText = text;
+        runOnUiThread(new Runnable() {
+
+            @Override
+            public void run() {
+                Toast.makeText(NoteActivity.this, toastText, Toast.LENGTH_SHORT).show();
             }
         });
     }
